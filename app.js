b51ecdc6923c0c6dbda163037c965e6ac04bb168
runningtalus <dave@davemay.org>
2014-05-06 12:25:10 -0600

    included Moment.js npm module to render time

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var app = express();
+var moment = require('moment');
+
+// app.get('/', function(req, res) {
+// 	res.send('<h1>Hello Boulder</h1>');
+// });
+
+// app.get('/date', function(req, res) {
+// 	res.send(new Date());
+// });
+
+console.log('outside');
 
 app.get('/', function(req, res) {
-	res.send('<h1>Hello Boulder</h1>');
-});
-
-app.get('/date', function(req, res) {
-	res.send(new Date());
+	res.send( moment().format('MMMM Do YYYY, h:mm:ss a') );
+	console.log('inside');
 });
 
 var server = app.listen(6678, function() {
